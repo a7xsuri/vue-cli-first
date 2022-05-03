79e393a2bbf7cc182221e8f1e8c3fa3cc671c3f3
a7xsuri <leonardofilipposurico@gmail.com>
2022-05-03 23:06:12 +0200

    finish

---
+++ nuovoprogetto/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ExTitle/>
+    <ExHero/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ExTitle from './components/ExTitle.vue'
+import ExHero from './components/ExHero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ExTitle,
+    ExHero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ nuovoprogetto/src/components/ExHero.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="jumbotron">
+        <img src="../assets/ottobre.jpg" alt="ciao">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExHero'
+}
+</script>
+
+<style scoped>
+
+</style>
